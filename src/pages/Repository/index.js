707834c7443f20bde78f0d5cd34eeb8ea6edf880
llucasmota <lucas.o.mota@gmail.com>
2020-03-22 19:29:25 -0300

    Alterando index.js referente ao rota Repository para um componente de classe, bem como adicionando request para issues

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+export default class Repository extends Component {
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`),
+    ]);
+
+    console.log(repository);
+    console.log(issues);
+  }
+
+  render() {
+    return <h1>Repository:</h1>;
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Repository from './pages/Repository';
 
+/**
+ * através do path="/repository/:repository" recupero
+ * os parâmetros que enviei através do link da página Main
+ */
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
-        <Route path="/repository" component={Repository} />
+        <Route path="/repository/:repository" component={Repository} />
       </Switch>
     </BrowserRouter>
   );
