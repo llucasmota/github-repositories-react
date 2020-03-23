f5205d37ec5a848aa71dfec9a7ca87aa959e37db
llucasmota <lucas.o.mota@gmail.com>
2020-03-23 13:27:41 -0300

    Adicionando biblioteca propTypes para validação dos dados que são enviados nas rotas, bem como implementação desses schema no arquito index de Repository

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,23 @@
+/* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired(),
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
   async componentDidMount() {
     const { match } = this.props;
 
@@ -9,14 +25,24 @@ export default class Repository extends Component {
 
     const [repository, issues] = await Promise.all([
       api.get(`/repos/${repoName}`),
-      api.get(`/repos/${repoName}/issues`),
+      api.get(`/repos/${repoName}/issues`, {
+        state: 'open',
+        per_page: 5,
+      }),
     ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
 
     console.log(repository);
     console.log(issues);
   }
 
   render() {
+    const { repository, issues, loading } = this.state;
     return <h1>Repository:</h1>;
   }
 }
