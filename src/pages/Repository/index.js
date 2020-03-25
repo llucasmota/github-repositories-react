a5082b31b6f2a14cc1a53273e579678e36189c9e
llucasmota <lucas.o.mota@gmail.com>
2020-03-24 21:45:40 -0300

    Ajustando tratamento de schema(propTypes)do componente

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,17 +1,13 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/static-property-placement */
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-export default class Repository extends Component {
-  static propTypes = {
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        repository: PropTypes.string,
-      }),
-    }).isRequired(),
-  };
+import { Loading } from './styles';
 
+export default class Repository extends Component {
   state = {
     repository: {},
     issues: [],
@@ -43,6 +39,17 @@ export default class Repository extends Component {
 
   render() {
     const { repository, issues, loading } = this.state;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
     return <h1>Repository:</h1>;
   }
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
